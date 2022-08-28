9ff365475c7dc44879b87e12eeeb519b554d32ce
xtadeushx <alekscherkasov13@gmail.com>
2022-08-28 11:11:43 +0300

    Fill: add hero  section with styles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from '../style';
 import { discount, robot } from '../assets';
-import { GetStarted } from "./GetStarted";
+import { GetStarted } from './GetStarted';
 
 const Hero = () => (
   <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY} `}>
@@ -14,14 +14,31 @@ const Hero = () => (
         </p>
       </div>
       <div className="flex flex-row justify-between w-full">
-        <h1 className='flex-1 font-poppins fomnt-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]'>
-          The Next Payment <br className="sm:block hidden"/> {" "}
-          <span className="text-gradient">Generation</span> <br className="sm:block hidden"/> {" "}
-           Method.</h1>
-           <div className="ss:flex hidden md:mr=4">
-            <GetStarted/>
-           </div>
+        <h1 className="flex-1 font-poppins fomnt-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]">
+          The Next Payment <br className="sm:block hidden" />{' '}
+          <span className="text-gradient">Generation</span> <br className="sm:block hidden" />{' '}
+        </h1>
+        <div className="ss:flex hidden md:mr=4">
+          <GetStarted />
+        </div>
       </div>
+      <h1 className=" font-poppins fomnt-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full">
+        Payment Method.
+      </h1>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Our team of experts uses a methodology to identify the credit cards most likely to fit your
+        needs. We examine annual percentage rates, annual fees.
+      </p>
+    </div>
+
+    <div className={`flex-1 ${styles.flexCenter} md:my-0 my-10 relative`}>
+      <img src={robot} alt="billing" className="w-[100%]  h-[100%] relative z-[5]" />
+      <div className="absolute z-0 w-[40%] h-[35%] top-0 pink__gradient" />
+      <div className="absolute z-1 w-[80%] h-[80%] bottom-40 white__gradient rounded-full" />
+      <div className="absolute z-0 w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
+    </div>
+    <div className={`ss:hidden ${styles.flexCenter}`}>
+    <GetStarted />
     </div>
   </section>
 );
